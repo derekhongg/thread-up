27e52da30edf7a73f76d6153473b8e36c2bb29c8
Derek Hong <derek.hong@paccar.com>
2023-08-10 15:44:55 -0700

    new updates

--- client/src/components/customMessageForms/standardMessageForm.js
+++ client/src/components/customMessageForms/standardMessageForm.js
@@ -1,8 +1,41 @@
 import React, { useState } from "react";
 import MessageFormUI from "./MessageFormUI";
+import { XMarkIcon } from "@heroicons/react/24/solid";
 
 const standardMessageForm = () => {
-    return 
+
+    const [message, setMessage] = useState("");
+    const [attachment, setAttachment] = useState("");
+    const [preview, setPreview] = useState("");
+
+    return
+    <div className = "message-form-container">
+        {preview && (
+            <div>
+                <img
+                    alt="message-form-preview"
+                    className="message-form-preview-image"
+                    src={preview}
+                    onLoad={() => URL.revokeObjectURL(preview)}
+                />
+                <XMarkIcon
+                    className="message-form-icon-x"
+                    onClick = {() => {
+                        setPreview("");
+                        setAttachment("");
+                    }}
+
+                />
+            </div>
+        )}
+    </div>
 }
 
 export default standardMessageForm;
+
+
+/* Notes:
+
+- URL.revokeObjectURL will free up space when you don't need the image anymore
+
+*/
