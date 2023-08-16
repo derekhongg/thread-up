03a29d7cf969e0f434ccfa6c703a112423f7c88d
derekhongg <hongderek@gmail.com>
2023-08-15 19:55:22 -0700

    set up ai on frontend

--- client/src/components/customMessageForms/ai.jsx
+++ client/src/components/customMessageForms/ai.jsx
@@ -4,7 +4,13 @@ import MessageFormUI from "./MessageFormUI";
 
 import React from 'react'
 
-const ai = () => {
+const ai = ({ props, activeChat }) => {
+    const [message, setMessage] = useState("");
+    const [attachment, setAttachment] = useState("");
+    const [trigger] = usePostAiTextMutation();
+
+    const handleChange = (e) => setMessage(e.target.value);
+
     return (
         <div>ai</div>
     )
